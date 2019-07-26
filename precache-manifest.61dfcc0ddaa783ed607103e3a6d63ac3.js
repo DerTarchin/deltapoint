ef62c9e87aa694a2e2adc446b95f3546444775ae
Hizal Celik <hizalcelik@gmail.com>
2019-07-26 08:09:37 -0400

    Updates

---
+++ precache-manifest.61dfcc0ddaa783ed607103e3a6d63ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e7d85d825f130cc8410ade9a642521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47e7ef444500b2ca434d",
+    "url": "/static/css/main.56b29560.chunk.css"
+  },
+  {
+    "revision": "189e2411f0ca1ec73743",
+    "url": "/static/js/2.445ca5a6.chunk.js"
+  },
+  {
+    "revision": "47e7ef444500b2ca434d",
+    "url": "/static/js/main.d6e64a68.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "75c537f921a32b1f032f63e26c544b20",
+    "url": "/static/media/logo.75c537f9.svg"
+  }
+]);
