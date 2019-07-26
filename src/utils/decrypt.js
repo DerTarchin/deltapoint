aa2aeae815c692cbff7a7b32d3f732aceec2f679
Hizal Celik <hizalcelik@gmail.com>
2019-07-26 07:43:10 -0400

    updated decrypter

--- src/utils/decrypt.js
+++ src/utils/decrypt.js
@@ -5,6 +5,18 @@ export const decrypt = (message, passphrase) => {
         encryptedHTML = message.substring(64),
         decryptedHMAC = CryptoJS.HmacSHA256(encryptedHTML, CryptoJS.SHA256(passphrase).toString()).toString();
 
-  if (decryptedHMAC !== encryptedHMAC) return;
-  return JSON.parse(CryptoJS.AES.decrypt(encryptedHTML, passphrase).toString(CryptoJS.enc.Utf8));
+  if (decryptedHMAC !== encryptedHMAC) return; // incorrect pass
+
+  const keySize = 256/32, 
+        iterations = 1000,
+        salt = CryptoJS.enc.Hex.parse(encryptedHTML.substr(0, 32)),
+        iv = CryptoJS.enc.Hex.parse(encryptedHTML.substr(32, 32)),
+        encrypted = encryptedHTML.substring(64),
+        key = CryptoJS.PBKDF2(passphrase, salt, { keySize, iterations });
+
+  return JSON.parse(CryptoJS.AES.decrypt(encrypted, key, {
+      iv: iv,
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC
+  }).toString(CryptoJS.enc.Utf8));
 }
